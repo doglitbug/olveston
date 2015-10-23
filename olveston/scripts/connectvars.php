103f62f307aadc980685edfcfe836dab337bab01
Arron Dick <dickaj1@student.op.ac.nz>
2015-10-23 14:52:17 +1300

    Update connectvars.php

--- olveston/scripts/connectvars.php
+++ olveston/scripts/connectvars.php
@@ -1,9 +1,9 @@
 <?php
 //Define database connection constants
-//This is the file for usage on laptop
+//You will need to edit USERNAME and PASSWORD!!!
 define('DB_HOST','localhost');
 define('DB_PORT','3306');
-define('DB_USER','dbuser');
-define('DB_PASSWORD','22applesAA');
+define('DB_USER','USERNAME');
+define('DB_PASSWORD','PASSWORD');
 define('DB_NAME','dickaj1_olveston');
 ?>
